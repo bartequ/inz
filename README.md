612a85a1fcd8bb09ee44e341428e551b2127fcdf
bartequ <bartlomiej.szabat@gmail.com>
2019-03-04 18:47:47 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
-Name of the 'MZ_Centracom.tndb' was changed to 'test1.tndb', 'MZ_AGG_SW_Test.tndb' to 'test.tndb' to simplify testing. File cytoscape.min.js, cytoscape-qtip.js are files from Cytoscape.js library. To run appllication download files, start server e.g. Apache and type into browser localhost:port/YOUR_CATALOG. Default port is 8080.
+Land_Mobile_Radio_system_structure.tndb file contains description of network structure in XML . To run appllication download files, start server e.g. Apache and type into browser localhost:port/YOUR_CATALOG. Default port is 8080.
 File converter.js contains all of the app funcionality, cy-style.json and style.css contain information about styles.
 
 &lt;div id="cy"&gt;&lt;/div&gt; is holder for cytoscape object.
 
-Files test.tndb (or test1.tnsb) is fetched from local folder.
+Input file is fetched from local folder.
 
-1) First step is loading test.tndb. After file is loaded function loadJsonData() creates:
- - Array loadStructure which contains informations about nodes, loadJsonInterZoneLinks() and loadJsonIntraZoneLinks() push edges and cytoscape attribute 'elements' load data from that Array,
- - Array zonesNumberNames used by loadJsonInterZoneLinks() and loadJsonIntraZoneLinks(),
- - cluster - Array contains clusters used by  loadJsonIntraZoneLinks()
- 
- 2) Function loadJsonInterZoneLinks() is adding edges to loadStructure and forward cluster to loadJsonIntraZoneLinks(). loadJsonInterZoneLinks() is invoked second because it needs 'rs' parameter which loadJsonData() returns.
- 
- 3) Function loadJsonIntraZoneLinks() is adding edges to loadStructure
+File is divided into 3 main functions which call other function to realize subtasks.
+
+- loadStructure() is responsible for fetch data and prepare to draw by transformating to different form
+- loadGraphStyle() is responsible for fetch graph style (which is different than main application style file)
+- drawNetwork() is responsible for show network topology in proper form (uses cytoscape.js library)
--- converter.js
+++ converter.js
@@ -1,5 +1,8 @@
+const GRAPH_FILE = "Land_Mobile_Radio_system_structure.tndb";
+const GRAPH_STYLE = "cy-style.json";
+
 function findObjectByKey(array, key, value) {
-  for (var i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     if (array[i][key] === value) {
       return array[i];
     }
@@ -8,42 +11,42 @@ function findObjectByKey(array, key, value) {
 }
 
 function xmlToJson(xml) {
-  var obj = {};
+  let obj = {};
 
-  if (xml.nodeType == 1) {
+  if (xml.nodeType === 1) {
     if (xml.attributes.length > 0) {
-      for (var j = 0; j < xml.attributes.length; j++) {
-        var attribute = xml.attributes.item(j);
+      for (let j = 0; j < xml.attributes.length; j++) {
+        let attribute = xml.attributes.item(j);
         obj[attribute.nodeName] = attribute.nodeValue;
       }
     }
-  } else if (xml.nodeType == 3) {
+  } else if (xml.nodeType === 3) {
     obj = xml.nodeValue.trim(); 
   }
 
   if (xml.hasChildNodes()) {
-    for (var i = 0; i < xml.childNodes.length; i++) {
-      var item = xml.childNodes.item(i);
-      var nodeName = item.nodeName;
-      if (typeof(obj[nodeName]) == "undefined") {
-        var tmp = xmlToJson(item);
+    for (let i = 0; i < xml.childNodes.length; i++) {
+      let item = xml.childNodes.item(i);
+      let nodeName = item.nodeName;
+      if (typeof(obj[nodeName]) === "undefined") {
+        let tmp = xmlToJson(item);
         if (tmp !== "") 
           obj[nodeName] = tmp;
       } else {
-        if (typeof(obj[nodeName].push) == "undefined") {
-          var old = obj[nodeName];
+        if (typeof(obj[nodeName].push) === "undefined") {
+          let old = obj[nodeName];
           obj[nodeName] = [];
           obj[nodeName].push(old);
         }
-        var tmp = xmlToJson(item);
+        let tmp = xmlToJson(item);
         if (tmp !== "") 
           obj[nodeName].push(tmp);
       }
     }
   }
-  if (!Array.isArray(obj) && typeof obj == 'object') {
-    var keys = Object.keys(obj);
-    if (keys.length == 1 && keys[0] == '#text') return obj['#text'];
+  if (!Array.isArray(obj) && typeof obj === 'object') {
+    let keys = Object.keys(obj);
+    if (keys.length === 1 && keys[0] === '#text') return obj['#text'];
     if (keys.length === 0) return null;
   }
   return obj;
@@ -51,13 +54,13 @@ function xmlToJson(xml) {
 
 function loadJsonData(rs) {
 
-  var zonesNumberNames = [];
-  var cluster = [];
-  var loadStructure = [];
-  var mso = [];
-  var zone = [];
-  var site = [];
-  var clusterLength;
+  let zonesNumberNames = [];
+  let cluster = [];
+  let loadStructure = [];
+  let mso = [];
+  let zone = [];
+  let site = [];
+  let clusterLength;
 
     if (!Array.isArray(rs.cluster))  {
       loadStructure.push({group: "nodes", data: {id: rs.cluster.id, name: rs.cluster.name, number: rs.cluster.number, radiosystemid: rs.cluster.radiosystemid,
@@ -67,16 +70,16 @@ function loadJsonData(rs) {
     }
     else {
       clusterLength = rs.cluster.length;
-      for (var a=0; a<clusterLength; a++) {
+      for (let a=0; a<clusterLength; a++) {
         cluster.push(rs.cluster[a]);
       }
     }
-    for (var i=0; i<clusterLength; i++){
+    for (let i=0; i<clusterLength; i++){
       if (clusterLength > 1){
         loadStructure.push({group: "nodes", data: {id: cluster[i].id, name: rs.cluster[i].name, number: rs.cluster[i].number, radiosystemid: rs.cluster[i].radiosystemid,
                             label: rs.cluster[i].name}, classes: 'cluster'});
       }
-        var msoLength;
+        let msoLength;
         if (!Array.isArray(cluster[i].mso)){
           loadStructure.push({group: "nodes", data: {id: cluster[i].mso.id, parent: cluster[i].id, cenpresent: cluster[i].mso.cenpresent, name: cluster[i].mso.name, 
                               number: cluster[i].mso.number, label: cluster[i].mso.name}, classes: 'mso'});
@@ -84,11 +87,11 @@ function loadJsonData(rs) {
         }
         else {
           msoLength = cluster[i].mso.length; 
-          for (var a=0; a<msoLength; a++){
+          for (let a=0; a<msoLength; a++){
             mso.push(cluster[i].mso[a]);
           }
         }  
-        for (var j=0; j<msoLength; j++){
+        for (let j=0; j<msoLength; j++){
           if (msoLength > 1){
             loadStructure.push({group: "nodes", data: {id: mso[j].id, parent: cluster[i].id, cenpresent: mso[j].cenpresent, name: mso[j].name, 
                                 number: mso[j].number, label: mso[j].name}, classes: 'mso'});
@@ -97,7 +100,7 @@ function loadJsonData(rs) {
         }
         mso = [];
 
-          var zoneLength;
+          let zoneLength;
           if (!Array.isArray(cluster[i].zone)){
             loadStructure.push({group: "nodes", data: {id: cluster[i].zone.id, parent: cluster[i].zone.msoid, clusterid: cluster[i].zone.clusterid, name: cluster[i].zone.name, 
                                 number:cluster[i].zone.number, siteLink: cluster[i].zone.siteLink, label: cluster[i].zone.name}, classes: 'zone'});
@@ -107,11 +110,11 @@ function loadJsonData(rs) {
           }
           else {
             zoneLength = cluster[i].zone.length; 
-            for (var a=0; a<zoneLength; a++){
+            for (let a=0; a<zoneLength; a++){
               zone.push(cluster[i].zone[a]);
             }
           }
-          for (var k=0; k<zoneLength; k++){
+          for (let k=0; k<zoneLength; k++){
             if (zoneLength > 1) {
               loadStructure.push({group: "nodes", data: {id: zone[k].id, parent: zone[k].msoid, clusterid: zone[k].clusterid, name: zone[k].name, 
                                   number: zone[k].number, siteLink: zone[k].siteLink, label: zone[k].name}, classes:'zone'});
@@ -119,7 +122,7 @@ function loadJsonData(rs) {
               zone.push(cluster[i].zone[k]);
             }
     
-            var siteLength;
+            let siteLength;
             if (!Array.isArray(zone[k].site)){
               loadStructure.push({group: "nodes", data: {id: zone[k].site.id, parent: zone[k].site.zoneid, name: zone[k].site.name, number: zone[k].site.number, 
                                   siteLink: zone[k].site.siteLink}, classes: "site" });
@@ -127,11 +130,11 @@ function loadJsonData(rs) {
             }
             else {  
               siteLength = zone[k].site.length;   
-              for (var a=0; a<siteLength; a++){
+              for (let a=0; a<siteLength; a++){
                 site.push(zone[k].site[a]); 
               }
             }
-            for (var m=0; m<siteLength; m++){
+            for (let m=0; m<siteLength; m++){
               if (siteLength > 1) {
                 loadStructure.push({group: "nodes", data: {id: site[m].id, parent: site[m].zoneid, name: site[m].name, number: site[m].number, 
                                     siteLink: site[m].siteLink}, classes:'site' });
@@ -142,7 +145,7 @@ function loadJsonData(rs) {
           }
           zone = [];
     }
-    //console.log(zonesNumberNames);
+
     return new Promise(function(resolve, reject) {
       resolve([rs, loadStructure, zonesNumberNames, cluster]);
     });
@@ -152,8 +155,8 @@ function loadJsonInterZoneLinks(rs, loadStructure, zonesNumberNames, cluster){
       
   if (Array.isArray(rs.interzonelinkbundle)) {
     for (i=0; i<rs.interzonelinkbundle.length; i++) {
-      var zoneA = rs.interzonelinkbundle[i].zonea;
-      var zoneB = rs.interzonelinkbundle[i].zoneb;
+      let zoneA = rs.interzonelinkbundle[i].zonea;
+      let zoneB = rs.interzonelinkbundle[i].zoneb;
       loadStructure.push({group: "edges", data: { id: rs.interzonelinkbundle[i].id, source: findObjectByKey(zonesNumberNames, 'number', zoneA).id, 
                           target: findObjectByKey(zonesNumberNames, 'number', zoneB).id }, classes: 'interZoneLink'});
     }
@@ -165,32 +168,32 @@ function loadJsonInterZoneLinks(rs, loadStructure, zonesNumberNames, cluster){
 
 function loadJsonIntraZoneLinks(loadStructure, zonesNumberNames, cluster) {
 
-  var mso = [];
-    for (var i=0; i<cluster.length; i++){
-      var msoLength;
+  let mso = [];
+    for (let i=0; i<cluster.length; i++){
+      let msoLength;
       if (!Array.isArray(cluster[i].mso)){
         msoLength = 1;
         mso.push(cluster[i].mso);
       }
       else {
-        var msoLength = cluster[i].mso.length;
-        for (var a=0; a<cluster[i].mso.length; a++){
+        msoLength = cluster[i].mso.length;
+        for (let a=0; a<cluster[i].mso.length; a++){
           mso.push(cluster[i].mso[a]);
         }
       }  
             
-      for (var j=0; j<msoLength; j++){
+      for (let j=0; j<msoLength; j++){
         if ('intraZoneLink' in mso[j] === true) {  
           if (!Array.isArray(mso[j].intraZoneLink)) {
-            var zoneA = mso[j].intraZoneLink.zoneAId;
-            var zoneB = mso[j].intraZoneLink.zoneBId;
+            let zoneA = mso[j].intraZoneLink.zoneAId;
+            let zoneB = mso[j].intraZoneLink.zoneBId;
             loadStructure.push({group: "edges", data: { id: mso[j].intraZoneLink.id, source: findObjectByKey(zonesNumberNames, 'number', zoneA).id, 
                                 target: findObjectByKey(zonesNumberNames, 'number', zoneB).id }, classes: 'intraZoneLink'});
           }
           else {
-            for (var k=0; k<mso[j].intraZoneLink.length; k++) {
-              var zoneA = mso[j].intraZoneLink[k].zoneAId;
-              var zoneB = mso[j].intraZoneLink[k].zoneBId;
+            for (let k=0; k<mso[j].intraZoneLink.length; k++) {
+              let zoneA = mso[j].intraZoneLink[k].zoneAId;
+              let zoneB = mso[j].intraZoneLink[k].zoneBId;
               loadStructure.push({group: "edges", data: { id: mso[j].intraZoneLink[k].id, source: findObjectByKey(zonesNumberNames, 'number', zoneA).id, 
                                   target: findObjectByKey(zonesNumberNames, 'number', zoneB).id }, classes: 'intraZoneLink'});
             }
@@ -200,152 +203,202 @@ function loadJsonIntraZoneLinks(loadStructure, zonesNumberNames, cluster) {
       mso = [];
     }
   return loadStructure;
-}  
-
-
-Promise.all([
-fetch('test.tndb')
-.then(response => response.text())
-.then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-.then(function(dataXml){
-    return xmlToJson(dataXml);
-})
-.then(function(dataJson){
-  var rs = dataJson.DLRadioSystem;
-  return loadJsonData(rs);
-})
-.then(function(dataNodes) {
-  return loadJsonInterZoneLinks(dataNodes[0], dataNodes[1], dataNodes[2], dataNodes[3]);
-})
-.then(function(dataNodesEdges) {
-  return loadJsonIntraZoneLinks(dataNodesEdges[0], dataNodesEdges[1], dataNodesEdges[2]);
-}),
-    
-fetch('cy-style.json', {mode: 'no-cors'})
-  .then(function(res) {
-    return res.json()
-  })
-])
-.then(function(dataArray) {
-  data = dataArray[0];
-  style = dataArray[1];
-
-  var nodes = [];
-  var edges = [];
-  for (var i=0; i<data.length; i++) {
-    if (data[i].group == 'nodes') {
-      nodes.push(data[i]);
-    }
-    else if (data[i].group == 'edges') {
-      edges.push(data[i]);
-    }
-  }
-  var elements = {nodes, edges};
-
-  console.log(elements);
-
-  var cy = window.cy = cytoscape({
-    container: document.getElementById('cy'),
-
-    style: style,
-    
-    elements: elements,
-
-    ready: function(){
-      window.cy = this;
-    }
-  });
-
-  var layout = cy.layout({name: 'cose-bilkent'});
-  layout.run();
-
-function highlight( node ){
-
-  var nodeId = node.id();
-  var childNodes = cy.nodes('node[parent="'+nodeId+'"]');
-  var nhood = lastHighlighted = childNodes;
-  var others = lastUnhighlighted = cy.elements().not( nhood );
-
-
-  //others.style("visibility", "hidden");
-  nhood.style("visibility", "visible");
-
-    var layoutZoom = nhood.layout({
-        name: 'cose-bilkent',
-        elements: nhood
-    });
-
-    layout.stop();
-    layoutZoom.run();
-
 }
 
-function clear(){
-    cy.elements().style("visibility", "visible");
+function divideStructureIntoNodesAndEdges(data) {
+    let nodes = [];
+    let edges = [];
+    for (let i=0; i<data.length; i++) {
+        if (data[i].group === 'nodes') {
+            nodes.push(data[i]);
+        }
+        else if (data[i].group === 'edges') {
+            edges.push(data[i]);
+        }
+    }
+    let x = {nodes, edges};
+    console.log(x);
+    return {nodes, edges};
+}
+
+function assignPosition(elements, cy) {
+    let nodesPosition = cy.nodes().map(node => node.position());
+
+    if (nodesPosition.length === elements.nodes.length) {
+        for (let i=0; i<nodesPosition.length; i++) {
+            elements.nodes[i].position = {
+                x: nodesPosition[i].x,
+                y: nodesPosition[i].y
+            };
+            elements.nodes[i].data.orgPos = {
+                x: nodesPosition[i].x,
+                y: nodesPosition[i].y
+            };
+        }
+    }
+}
+
+function loadStructure() {
+
+        return fetch(GRAPH_FILE)
+            .then(response => response.text())
+            .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+            .then(function (dataXml) {
+                return xmlToJson(dataXml);
+            })
+            .then(function (dataJson) {
+                return loadJsonData(dataJson.DLRadioSystem);
+            })
+            .then(function (dataNodes) {
+                return loadJsonInterZoneLinks(dataNodes[0], dataNodes[1], dataNodes[2], dataNodes[3])
+            })
+            .then(function (dataNodesEdges) {
+                return loadJsonIntraZoneLinks(dataNodesEdges[0], dataNodesEdges[1], dataNodesEdges[2]);
+            });
+}
+
+function loadGraphStyle() {
+
+        return fetch(GRAPH_STYLE, {mode: 'no-cors'})
+            .then(function (res) {
+                return res.json()
+            });
+}
+
+function drawNetwork(dataArray) {
+
+    const data = dataArray[0];
+    const style = dataArray[1];
+    let elements = divideStructureIntoNodesAndEdges(data);
+
+    const cy = window.cy = cytoscape({
+        container: document.getElementById('cy'),
+
+        style: style,
+
+        elements: elements,
+
+        ready: function(){
+            window.cy = this;
+        }
+    });
+    const layout = cy.layout({name: 'cose-bilkent'});
     layout.run();
-}
 
-cy.on('select unselect', 'node', function(e){
-  var node = cy.$('node:selected');
+    assignPosition(elements, cy);
 
-  if(node.nonempty()){
-    Promise.resolve().then(function(){
-      return highlight(node);
+    cy.on('select unselect', 'node', function(){
+        let node = cy.$('node:selected');
+
+        if(node.nonempty()){
+            Promise.resolve().then(function(){
+                return highlight(node);
+            });
+        }
+        else {
+            clear();
+        }
     });
-  }
-  else {
-    clear();
-  }
-});
 
-cy.$('.site').qtip({
-    content: function(){
-        for (var i=0; i<data.length; i++){
-            if (this.id() == data[i].data.id)
-                return 'site number: ' + data[i].data.number + '<br>siteLink: ' + data[i].data.siteLink;
+    cy.$('.site').qtip({
+        content: function(){
+            for (let i=0; i<data.length; i++){
+                if (this.id() === data[i].data.id)
+                    return 'site number: ' + data[i].data.number + '<br>siteLink: ' + data[i].data.siteLink;
+            }
+        },
+        position: {
+            target: 'mouse',
+            my: 'top right',
+            adjust: {
+                mouse: false
+            }
+        },
+        style: {
+            classes: 'qtip-bootstrap',
+        },
+        show: {
+            event: 'cxttapend'
         }
-    },
-    position: {
-        target: 'mouse',
-        my: 'top right',
-        adjust: {
-            mouse: false
-        }
-    },
-    style: {
-        classes: 'qtip-bootstrap',
-    },
-    show: {
-        event: 'cxttapend'
-    }
-});
+    });
 
-cy.$('.zone').qtip({
-    content: function(){
-        for (var i=0; i<data.length; i++){
-            if (this.id() == data[i].data.id){
-                var description = "siteLinks:<br>";
-                if (Array.isArray(data[i].data.siteLink)) {
-                    for (var j=0; j<data[i].data.siteLink.length; j++) {
-                        description += "id: " + data[i].data.siteLink[j].id + "<br>" + "number: " + data[i].data.siteLink[j].number + "<br><br>";
+    cy.$('.zone').qtip({
+        content: function(){
+            for (let i=0; i<data.length; i++){
+                if (this.id() === data[i].data.id){
+                    let description = "siteLinks:<br>";
+                    if (Array.isArray(data[i].data.siteLink)) {
+                        for (let j=0; j<data[i].data.siteLink.length; j++) {
+                            description += "id: " + data[i].data.siteLink[j].id + "<br>" + "number: " + data[i].data.siteLink[j].number + "<br><br>";
+                        }
+                        return description;
                     }
-                    return description;
+                }
+            }
+        },
+        position: {
+            my: 'left center',
+            target: 'mouse',
+            adjust: {
+                mouse: false
+            }
+        },
+        style: {
+            classes: 'qtip-bootstrap',
+        },
+        show: {
+            event: 'cxttapend'
+        }
+    });
+
+    function highlight(node){
+
+        let nhood = cy.nodes('node[parent="'+node.id()+'"]');
+        let others = cy.elements().not(nhood);
+
+        cy.edges().style("visibility", "hidden");
+
+        let layoutZoom = nhood.layout({
+            name: 'grid',
+            elements: nhood
+        });
+        layout.stop();
+        layoutZoom.run();
+
+        nhood.style("visibility", "visible");
+        let othersBool = [];
+        for (let i=0; i<others.length; i++) {
+            othersBool[i] = false;
+        }
+
+        let ancestors = nhood[0].ancestors();
+        for (let j=0; j<ancestors.length; j++) {
+            for (let i=0; i<others.length; i++) {
+                if (ancestors[j].data().id === others[i].data().id) {
+                    others[i].style("visibility", "visible");
+                    othersBool[i] = true;
+                    continue;
+                }
+                else if (!othersBool[i]) {
+                    others[i].style("visibility", "hidden");
                 }
             }
         }
-    },
-    position: {
-        my: 'left center',
-        target: 'mouse',
-        adjust: {
-            mouse: false
-        }
-    },
-    style: {
-        classes: 'qtip-bootstrap',
-    },
-    show: {
-        event: 'cxttapend'
     }
-});
+
+    function clear(){
+        cy.elements().style("visibility", "visible");
+        layout.run();
+    }
+}
+
+Promise.all([
+        loadStructure(),
+        loadGraphStyle()
+])
+.catch(function(error) {
+    console.log("Loading files into application failed, " + error);
+})
+.then(function(dataArray) {
+    return drawNetwork(dataArray)
 });
